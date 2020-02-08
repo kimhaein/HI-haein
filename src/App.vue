2e73aac41e2ae234b2c3cb6869787dad40331238
haein <kendy93@sbcn.co.kr>
2020-02-08 14:11:50 +0900

    기본 폴더 구조 생성

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,52 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <!-- <div id="header">
+      <div>
+        <router-link to="/test">test</router-link>
+        <p>Using mustaches: {{ rawHtml }}</p>
+        <p>Using v-html directive: <span v-html="rawHtml"></span></p>
+        <div :id="dynamicId">123</div>
+        <button :disabled="isButtonDisabled">Button</button>
+        <a :[attributeName]="url">11313</a>
+        <div @click="doSomething">{{reversedMessage}}</div>
+        <div>{{reversedMessage}}</div>
+      </div>
+    </div> -->
+    <router-view />
   </div>
 </template>
 
 <script>
 export default {
   name: 'App',
+  data() {
+    return {
+      rawHtml: '<span>12312<span>',
+      test: '1234',
+      dynamicId: true,
+      isButtonDisabled: true,
+      attributeName: 'href',
+      url: 'https://www.naver.com',
+    };
+  },
+  computed: {
+    // reversedMessage:;;
+  },
+
 };
 </script>
 
 <style>
+html, body {
+  margin: 0;
+  height: 100%;
+  overflow: hidden;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
+  background: #2c3e50;
 }
 </style>
---
+++ src/views/Main.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="main">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  data() {
+    return {
+      msg: 'Main',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@import url('../assets/styles/common.css');
+</style>
